55e35bcbd3c5605e16b29afbde46225aa2615ee3
Mykyta Kotlyarov <m.kotljarov@gmail.com>
2018-11-01 10:55:04 +0200

    Build file update

--- docker.img/confluence/build.sh
+++ docker.img/confluence/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gcloud auth configure-docker
-docker build -t gcr.io/confluence-220616/jenkins:0.1 ../../docker.img/jenkins
-docker push gcr.io/confluence-220616/jenkins:0.1
+docker build -t gcr.io/confluence-220616/confluence:0.1 ../../docker.img/confluence
+docker push gcr.io/confluence-220616/confluence:0.1
